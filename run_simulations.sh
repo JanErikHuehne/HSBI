658d2188648a1bcee19f9662758e7faadf2620e7
Jan-Erik Huehne <jan.huehne@tum.de>
2024-05-02 08:34:52 +0000

    updated script

--- run_simulations.sh
+++ run_simulations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run_simulations.sh
-param1=$1
-param2=$2
-param3=$3
-echo "Running simulation test";
-echo "$(param1) $(param2) $(param3)";
+param1="$1"
+param2="$2"
+param3="$3"
+
+echo "Running simulation with parameters: $param1, $param2, $param3"
