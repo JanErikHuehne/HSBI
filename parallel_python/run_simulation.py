309b631b4896aa791d8134d63960e87bf2bfb934
Jan-Erik Huehne <jan.huehne@tum.de>
2024-05-25 15:28:55 +0000

    added all metrics

--- parallel_python/run_simulation.py
+++ parallel_python/run_simulation.py
@@ -10,6 +10,7 @@ import numpy as np
 import h5py
 from collections.abc import Iterable
 import time 
+from scipy.signal import correlate
 logging.basicConfig(level=logging.INFO,
                         format=f"run_simulation {HOST}(%(asctime)s) - %(levelname)s - %(message)s", datefmt="%H:%M:%S")
 logger = logging.getLogger(__name__)
@@ -59,8 +60,9 @@ def metrics(result):
     bin_size_medium = 0.01
     bin_size_small = 0.001
     window_view_auto_cov = 0.5
+    gmax = 20.0
 
-    def extract_neuron_spikes(self, spike_times, neuron_ids):
+    def extract_neuron_spikes(spike_times, neuron_ids):
             neuron_spikes = {}
             for i in range(len(neuron_ids)):
                 neuron_id = neuron_ids[i]
@@ -110,29 +112,214 @@ def metrics(result):
         w_trace = sim_data['weights_ee']['weights']
         return np.mean(w_trace[:, -1])
          
-
+    def w_creep(sim_data):
+        """ 
+        creep of the weights. Change of the mean weight between start and finish as percentage,
+        taken as the maximum compared between different plasticity connections
+        """
+        w_trace_ee = sim_data['weights_ee']['weights']
+        start_w_ee = np.mean(w_trace_ee[:,0])
+        end_w_ee = np.mean(w_trace_ee[:, -1])
+        if start_w_ee + end_w_ee > 0.1:
+             creep_ww = np.abs(2* (end_w_ee - start_w_ee) / (end_w_ee + start_w_ee))
+        else:
+             crep_ww = 0.0
+        w_trace_ie = sim_data['weights_ie']['weights']
+        start_w_ie = np.mean(w_trace_ie[:,0])
+        end_w_ie = np.mean(w_trace_ie[:, -1])
+        if start_w_ie + end_w_ie > 0.1:
+             creep_ie = np.abs(2* (end_w_ie - start_w_ie) / (end_w_ie + start_w_ie))
+        else:
+             crep_ie = 0.0
+        return max(creep_ww, crep_ie)
+    
+    def w_blow(sim_data):
+        """Percentage of weights that did blow up to a maximum value"""
+        f_blow = 0.0
+        for conn in ['weights_ee', "weights_ie"]:
+            weights = sim_data[conn]['weights']
+            # we get the maximum weight value of each trace
+            max_w = np.max(weights, axis=1)
+            min_w = np.max(weights, axis=1)
+            blow = np.sum(max_w > gmax)
+            vanish = np.sum(min_w < 0.001)
+            f_blow += (blow + vanish) / (len(max_w))
+        return f_blow / 2
+    
+    def cv_isi(sim_data):
+        """coefficient of variation of the interspike intervals of exitatory neuron population"""
+        spikes = sim_data['spikes_pi']
+        t_spikes = spikes['times']
+        neuron_ids = spikes['neurons']
+        grouped_spikes = extract_neuron_spikes(t_spikes, neuron_ids)
+        var_isi_val = []
+        for key,val in zip(grouped_spikes.keys(), grouped_spikes.values()):
+            isi = np.std(np.diff(val)) / np.mean(np.diff(val))
+            var_isi_val.append(isi)
+        return np.mean(var_isi_val)
+    
     def wief(sim_data):
         """final mean IE weight"""
         w_trace = sim_data['weights_ie']['weights']
         return np.mean(w_trace[:, -1])
     
+    def std_fr_s(sim_data):
+        """
+        Standard deviation of the firing rate over spacial domain  
+        """
+
+     
+        grouped_spikes_e =  extract_neuron_spikes(sim_data['spikes_pe']['times'], sim_data['spikes_pe']['neurons'])
+        grouped_spikes_i =  extract_neuron_spikes(sim_data['spikes_pi']['times'], sim_data['spikes_pi']['neurons'])
+       
+        all_rates = []
+        # exitatory
+        for spike_train in grouped_spikes_e.values():
+          all_rates.append(len(spike_train) / sim_data['runtime'])
+
+        # inhibitory
+        for spike_train in grouped_spikes_i.values():
+            all_rates.append(len(spike_train) / sim_data['runtime'])
+        
+        return np.std(np.array(all_rates))
+
+    def std_fr(sim_data):
+        """standard deviation of the firing rate
+            FR are computed over successive 1ms time windows, on which the standard deviation was computed
+        """
+        tbins = np.arange(sim_data['t_start'], sim_data['t_end'],bin_size_medium)
+        grouped_spikes_e =  extract_neuron_spikes(sim_data['spikes_pe']['times'], sim_data['spikes_pe']['neurons'])
+        grouped_spikes_i =  extract_neuron_spikes(sim_data['spikes_pi']['times'], sim_data['spikes_pi']['neurons'])
+        # exitatory population
+        stds = []
+        for spike_train in grouped_spikes_e.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            std = np.std(binned_spike_train)
+            stds.append(std)
+
+        # inhibitory population
+        for spike_train in grouped_spikes_i.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            std = np.std(binned_spike_train)
+            stds.append(std)
+        return np.mean(stds)
+
+    def auto_cov(sim_data):
+        """
+        autocov of the spike train
+        Spike trains are binned in a time window of 10ms
+        autocov for each neuron is computed and normalized between -500 and 500 ms
+        Mean area under the curve is computed and averaged over neurons
+        """
+        grouped_spikes_e = extract_neuron_spikes(sim_data['spikes_pe']['times'], sim_data['spikes_pe']['neurons'])
+        grouped_spikes_i = extract_neuron_spikes(sim_data['spikes_pi']['times'], sim_data['spikes_pi']['neurons'])
+        tbins = np.arange(sim_data['t_start'], sim_data['t_end'], bin_size_medium)
+        binned = np.empty(shape=(0, len(tbins)-1))
+        # exitatory
+        for spike_train in grouped_spikes_e.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            binned = np.vstack([binned, binned_spike_train])
+
+        # inhibitory
+        for spike_train in grouped_spikes_i.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            binned = np.vstack([binned, binned_spike_train])
+        # Correlate
+        lags = int( window_view_auto_cov / bin_size_medium)
+        aoc = []
+        for x in binned:
+            x_corr = correlate(x - x.mean(), x- x.mean(), mode='full')
+            # normalize
+            x_corr = np.abs(x_corr) / x_corr.max()
+            aoc.append(np.mean(x_corr[(x_corr.size//2-(lags)):(x_corr.size//2+(lags+1))]))
+        return np.mean(aoc)
+    
+    def averaged_fano_spatial(sim_data):
+        tbins = np.arange(sim_data['t_start'], sim_data['t_end'], bin_size_big)
+        grouped_spikes_e =  extract_neuron_spikes(sim_data['spikes_pe']['times'], sim_data['spikes_pe']['neurons'])
+        grouped_spikes_i =  extract_neuron_spikes(sim_data['spikes_pi']['times'], sim_data['spikes_pi']['neurons'])
+        binned = np.empty(shape=(0, len(tbins)-1))
+        
+        # exitatory
+        for spike_train in grouped_spikes_e.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            binned = np.vstack([binned, binned_spike_train])
+
+        # inhibitory
+        for spike_train in grouped_spikes_i.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            binned = np.vstack([binned, binned_spike_train])
+        
+        # compute the fano factor over each time window 
+        mean = np.mean(binned, axis=0)
+        var = np.var(binned, axis=0)
+        fano = var / mean
+        return np.mean(fano)
+    
+    def averaged_fano_time(sim_data):
+        """fano factor for each spike train averaged over the population
+            Binning the spike-trains over 100ms successive windows 
+            Fano Factor computed per neuron, then averaged over the population
+        """
+        tbins = np.arange(sim_data['t_start'], sim_data['t_end'],bin_size_big)
+
+        grouped_spikes_e =  extract_neuron_spikes(sim_data['spikes_pe']['times'], sim_data['spikes_pe']['neurons'])
+        grouped_spikes_i =  extract_neuron_spikes(sim_data['spikes_pi']['times'], sim_data['spikes_pi']['neurons'])
+        # exitatory population
+        ffs = []
+        for spike_train in grouped_spikes_e.values():
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            if np.sum(binned_spike_train) <= 3:
+                continue
+            mean = np.mean(binned_spike_train)
+            var = np.var(binned_spike_train)
+            ffs.append(var / mean)
+        
+        # inhibitory population
+        for spike_train in grouped_spikes_i.values():
+
+            binned_spike_train = np.histogram(spike_train, tbins)[0]
+            if np.sum(binned_spike_train) <= 3:
+                continue
+
+            mean = np.mean(binned_spike_train)
+            var = np.var(binned_spike_train)
+            ffs.append(var / mean)
+        fano = np.mean(ffs) 
+        return fano
+    
     result['rate_e'] = rate_e(result)
     result['rate_i'] = rate_i(result)
     result['wmean_ee'] = weef(result)
     result['wmean_ie'] = wief(result)
+    result['f_w-blow'] = w_blow(result)
+    result['cv_isi'] = cv_isi(result)
+    result['std_fr'] = std_fr(result)
+    result['std_rate_spatial'] = std_fr_s(result)
+    result['auto_cov'] = auto_cov(result)
+    result['mean_fano_s'] = averaged_fano_spatial(result)
+    result['mean_fano_t'] = averaged_fano_time(result)
+    # We delete the raw data so its not saved, simulations can be repeated with the given seed
     del result['weights_ee']
     del result['weights_ie']
+    del result['spikes_pi']
+    del result['spikes_pe']
     return result
+
 @time_function
-def simulation(sim_params, run_id):
+def simulation(sim_params, run_id, seed=None):
     b2.start_scope()
+    # set random seed
+    if seed is None:
+        seed = np.random.randint(low=1)
+    b2.seed(seed)
     """Shared network parameters"""
-    NE = 200
+    NE = 400
     NI = NE / 4
     input_num = 100
     input_freq = 30 # Hz
     sim_time = 25
-    gmax = 100.0
+    gmax = 20.0
     lr = 1e-2 
     Aminus = 1.0
     epsilon = 0.1
@@ -260,12 +447,13 @@ def simulation(sim_params, run_id):
                     }
     return  metrics({'run_parameters': sim_params,
          'spikes_pe': spikes['Pe'],
+                'seed' : seed,
                 'spikes_pi' : spikes['Pi'],
                 'weights_ie' : weights['ie'],
                 'weights_ee' : weights['ee'],
-                 'runtime' : sim_time,
-                 't_start' : 0,#pre_simtime,
-                 't_end' : sim_time,#pre_simtime+simtime,
+                 'runtime' : 5,
+                 't_start' : sim_time,#pre_simtime,
+                 't_end' : sim_time + 5,#pre_simtime+simtime,
                  'dt' : float(b2.defaultclock.dt)})
         
 if __name__ == "__main__":
