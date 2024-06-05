2c131777752106703088b38df6af9b0101458520
Jan-Erik Huehne <jan.huehne@tum.de>
2024-06-05 09:23:51 +0000

    bugfix

--- parallel_python/run_simulation_eval.py
+++ parallel_python/run_simulation_eval.py
@@ -412,6 +412,8 @@ def simulation(sim_params, run_id, seed=None, run_dir=None):
     pre_neurons =  np.array(con_ie.i)
     post_neurons = np.array(con_ie.j)
     # Create a sparse COO matrix representing the synaptic connections
+    print(post_neurons.max())
+    print(pre_neurons.max())
     weight_matrix_sparse = coo_matrix((weights, (post_neurons, pre_neurons)), shape=(100, 400))
     # Convert sparse matrix to a dense matrix
     weight_matrix = weight_matrix_sparse.toarray()
