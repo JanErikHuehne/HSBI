b47103da109d8dd990fee8d46f8d376afdf8a511
Jan-Erik Huehne <jan.huehne@tum.de>
2024-06-28 10:07:12 +0000

    debugging

--- analysis/network_analysis.py
+++ analysis/network_analysis.py
@@ -6,11 +6,12 @@ import logging
 import time 
 import numpy as np 
 
-import json 
+import json
+from pathlib import Path
 
 
 logging.basicConfig(level=logging.INFO,
-                        format=f"run_simulation {HOST}(%(asctime)s) - %(levelname)s - %(message)s", datefmt="%H:%M:%S")
+                    format=f"run_simulation {HOST}(%(asctime)s) - %(levelname)s - %(message)s", datefmt="%H:%M:%S")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 b2.prefs.codegen.target = "numpy"
@@ -31,11 +32,11 @@ def time_function(func):
 
 @time_function
 def network_run(sim_params, working_directory):
-    logger.info(f"Running in  {working_directory}")
+    logger.info(f"Running in {working_directory}")
     b2.start_scope()
     """Shared network parameters"""
     NE = 400
-    NI = NE / 4
+    NI = NE // 4
     input_num = 40
     input_freq = 30 # Hz
     sim_time = 120
@@ -46,16 +47,16 @@ def network_run(sim_params, working_directory):
     gl = 10 * nS
     er = -80 * mV
     el = - 60 * mV
-    tau_gaba = 10.0 *ms
+    tau_gaba = 10.0 * ms
     tau_ampa = 5.0 * ms
     vt = -50 * mV
     memc = 200 * pfarad
-    eqs_neurons='''
+    eqs_neurons = '''
                 dv/dt=(-gl*(v-el)-(g_ampa*v+g_gaba*(v-er)))/memc : volt (unless refractory)
                 dg_ampa/dt = -g_ampa/tau_ampa : siemens
                 dg_gaba/dt = -g_gaba/tau_gaba : siemens
                 '''
-    neurons = b2.NeuronGroup(NE+NI, model=eqs_neurons, threshold='v > vt',
+    neurons = b2.NeuronGroup(NE + NI, model=eqs_neurons, threshold='v > vt',
                              reset='v=el', refractory=5*ms, method='euler')
     Pe = neurons[:NE]
     Pi = neurons[NE:]
@@ -65,45 +66,45 @@ def network_run(sim_params, working_directory):
     ee_Aplus = sim_params[2]
     ee_tauplus_stdp = sim_params[3] * ms
     ee_tauminus_stdp = sim_params[4] * ms
-    factor_ee =sim_params[5]
+    factor_ee = sim_params[5]
     ee_Aminus = -1.0
-    synapse_model ='''
+    synapse_model = '''
                 w : 1
                 dee_trace_pre_plus/dt = -ee_trace_pre_plus / ee_tauplus_stdp : 1 (event-driven)
                 dee_trace_pre_minus/dt = -ee_trace_pre_minus / ee_tauminus_stdp : 1 (event-driven)
                 dee_trace_post_plus/dt = -ee_trace_post_plus / ee_tauplus_stdp : 1 (event-driven)
                 dee_trace_post_minus/dt = -ee_trace_post_minus / ee_tauminus_stdp : 1 (event-driven)
-    '''
+                '''
     con_ee = b2.Synapses(Pe, Pe, model=synapse_model,
-                                on_pre='''
-                                        g_ampa += w*nS
-                                        ee_trace_pre_plus += 1.0
-                                        ee_trace_pre_minus += 1.0
-                                        w = clip(w + lr * factor_ee * (ee_alpha_pre + ee_Aplus*ee_trace_post_plus + ee_Aminus * ee_trace_post_minus), 0, gmax)
-                                        ''',
-                                on_post='''
-                                        ee_trace_post_plus += 1
-                                        ee_trace_post_minus += 1
-                                        w = clip(w + lr * (ee_alpha_post + ee_Aplus*ee_trace_pre_plus + ee_Aminus * ee_trace_pre_minus), 0, gmax)
-                                        '''
-                                )
+                         on_pre='''
+                                g_ampa += w*nS
+                                ee_trace_pre_plus += 1.0
+                                ee_trace_pre_minus += 1.0
+                                w = clip(w + lr * factor_ee * (ee_alpha_pre + ee_Aplus*ee_trace_post_plus + ee_Aminus * ee_trace_post_minus), 0, gmax)
+                                ''',
+                         on_post='''
+                                ee_trace_post_plus += 1
+                                ee_trace_post_minus += 1
+                                w = clip(w + lr * (ee_alpha_post + ee_Aplus*ee_trace_pre_plus + ee_Aminus * ee_trace_pre_minus), 0, gmax)
+                                '''
+                         )
     con_ee.connect(p=epsilon, condition='i != j')
     con_ee.w = 0.2
     # EI Plasticity
     con_ei = b2.Synapses(Pe, Pi, on_pre="g_ampa += 0.2*nS")
-    con_ei.connect(p=epsilon,  condition='i != j')
-    #  II Plasticity
-    con_ii = b2.Synapses(Pi,Pi, on_pre="g_gaba += 1*nS")
-    con_ii.connect(p=epsilon,  condition='i != j')
-    # IE Plasiticty 
+    con_ei.connect(p=epsilon, condition='i != j')
+    # II Plasticity
+    con_ii = b2.Synapses(Pi, Pi, on_pre="g_gaba += 1*nS")
+    con_ii.connect(p=epsilon, condition='i != j')
+    # IE Plasticity 
     ie_alpha_pre = sim_params[6]
     ie_alpha_post = sim_params[7]
-    ie_Aplus =  sim_params[8]
+    ie_Aplus = sim_params[8]
     ie_tauplus_stdp = sim_params[9] * ms
     ie_tauminus_stdp = sim_params[10] * ms
-    factor_ie =sim_params[11]
+    factor_ie = sim_params[11]
     ie_Aminus = -1.0
-    synapse_model ='''
+    synapse_model = '''
                 w : 1
                 die_trace_pre_plus/dt = -ie_trace_pre_plus / ie_tauplus_stdp : 1 (event-driven)
                 die_trace_pre_minus/dt = -ie_trace_pre_minus / ie_tauminus_stdp : 1 (event-driven)
@@ -111,43 +112,44 @@ def network_run(sim_params, working_directory):
                 die_trace_post_minus/dt = -ie_trace_post_minus / ie_tauminus_stdp : 1 (event-driven)
                 '''
     con_ie = b2.Synapses(Pi, Pe, model=synapse_model,
-                                on_pre='''
-                                        g_gaba += w*nS
-                                        ie_trace_pre_plus += 1.0
-                                        ie_trace_pre_minus += 1.0
-                                        w = clip(w + lr * factor_ie * (ie_alpha_pre + ie_Aplus * ie_trace_post_plus + ie_Aminus * ie_trace_post_minus), 0, gmax)
-                                        ''',
-                                on_post='''
-                                        ie_trace_post_plus += 1
-                                        ie_trace_post_minus += 1
-                                        w = clip(w + lr * (ie_alpha_post + ie_Aplus * ie_trace_pre_plus + ie_Aminus * ie_trace_pre_minus), 0, gmax)
-                                        '''
-                        )
+                         on_pre='''
+                                g_gaba += w*nS
+                                ie_trace_pre_plus += 1.0
+                                ie_trace_pre_minus += 1.0
+                                w = clip(w + lr * factor_ie * (ie_alpha_pre + ie_Aplus * ie_trace_post_plus + ie_Aminus * ie_trace_post_minus), 0, gmax)
+                                ''',
+                         on_post='''
+                                ie_trace_post_plus += 1
+                                ie_trace_post_minus += 1
+                                w = clip(w + lr * (ie_alpha_post + ie_Aplus * ie_trace_pre_plus + ie_Aminus * ie_trace_pre_minus), 0, gmax)
+                                '''
+                         )
     con_ie.connect(p=epsilon, condition='i != j')
     con_ie.w = 1.0
     neurons.v = 0
-    P = b2.PoissonGroup(input_num, input_freq*Hz)
-    S = b2.PoissonInput(Pe, N=input_num, target_var="g_ampa", rate=20*Hz, weight=0.4*nS)
+    P = b2.PoissonGroup(input_num, input_freq * Hz)
+    S = b2.PoissonInput(Pe, N=input_num, target_var="g_ampa", rate=20 * Hz, weight=0.4 * nS)
     results = {}
-   
+
     b2.run(10 * second, report='text')
     # First Recording Window
     MPe_All = b2.SpikeMonitor(Pe)
     MPi_All = b2.SpikeMonitor(Pi)
-    W_IE = b2.StateMonitor(con_ie, 'w', record=True,dt=0.1*second)
-    W_EE = b2.StateMonitor(con_ee, 'w', record=True,dt=0.1*second)
+    W_IE = b2.StateMonitor(con_ie, 'w', record=True, dt=0.1 * second)
+    W_EE = b2.StateMonitor(con_ee, 'w', record=True, dt=0.1 * second)
     b2.run(5 * second)
     # We get the information of the spike monitors
 
-    results[0] = {'start' : 10,
-                  'end' : 40,
-                  'ee_times' : list(np.array(MPe_All.t, dtype=float)),
-                  'ee_neuron_ids':  list(np.array(MPe_All.i, dtype=int)),
-                  'ie_times' : list(np.array(MPi_All.t, dtype=float)), 
-                  'ie_neuron_ids' :list(np.array(MPi_All.i).astype(int)), 
-                   'w_ee' : np.array(W_EE.w).astype(float).tolist(),
-                   'w_ie' : np.array(W_IE.w).astype(float).tolist()
-                  }
+    results[0] = {
+        'start': 10,
+        'end': 40,
+        'ee_times': list(np.array(MPe_All.t / second, dtype=float)),  # Convert times to seconds
+        'ee_neuron_ids': list(np.array(MPe_All.i, dtype=int)),
+        'ie_times': list(np.array(MPi_All.t / second, dtype=float)),  # Convert times to seconds
+        'ie_neuron_ids': list(np.array(MPi_All.i, dtype=int)),
+        'w_ee': np.array(W_EE.w, dtype=float).tolist(),
+        'w_ie': np.array(W_IE.w, dtype=float).tolist()
+    }
 
     # We reset the spike monitors 
     del MPe_All, MPi_All, W_IE, W_EE
@@ -156,28 +158,26 @@ def network_run(sim_params, working_directory):
     # We set new spike and weight monitors 
     MPe_All = b2.SpikeMonitor(Pe)
     MPi_All = b2.SpikeMonitor(Pi)
-    W_IE = b2.StateMonitor(con_ie, 'w', record=True,dt=0.1*second)
-    W_EE = b2.StateMonitor(con_ee, 'w', record=True,dt=0.1*second)
+    W_IE = b2.StateMonitor(con_ie, 'w', record=True, dt=0.1 * second)
+    W_EE = b2.StateMonitor(con_ee, 'w', record=True, dt=0.1 * second)
     b2.run(15 * second, report='text')
-    results[1] = {'start' : 10,
-                  'end' : 40,
-                  'ee_times' : list(np.array(MPe_All.t).astype(float)),
-                  'ee_neuron_ids':  list(np.array(MPe_All.i).astype(int)),
-                  'ie_times' : list(np.array(MPi_All.t).astype(float)), 
-                  'ie_neuron_ids' :list(np.array(MPi_All.i).astype(int)), 
-                   'w_ee' : np.array(W_EE.w).astype(float).tolist(),
-                   'w_ie' : np.array(W_IE.w).astype(float).tolist()
-                  }
+    results[1] = {
+        'start': 10,
+        'end': 40,
+        'ee_times': list(np.array(MPe_All.t / second, dtype=float)),  # Convert times to seconds
+        'ee_neuron_ids': list(np.array(MPe_All.i, dtype=int)),
+        'ie_times': list(np.array(MPi_All.t / second, dtype=float)),  # Convert times to seconds
+        'ie_neuron_ids': list(np.array(MPi_All.i, dtype=int)),
+        'w_ee': np.array(W_EE.w, dtype=float).tolist(),
+        'w_ie': np.array(W_IE.w, dtype=float).tolist()
+    }
+
     # We get the information of the spike monitors
 
     # we save the results as well as return them 
-    save_file = working_directory / "analysis_run_raw.json"
+    save_file = Path(working_directory) / "analysis_run_raw.json"
     logger.info(f"Saving to {save_file}")
     with open(save_file, "w") as f:
         json.dump(results, f)
     return results
- 
-
-
-
 
